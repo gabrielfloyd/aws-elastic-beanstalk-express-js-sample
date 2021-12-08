c79243cce79b971d611b5617120b1899d98a2975
Gabriel Floyd <gtfloyd@amazon.com>
2021-12-08 13:41:42 -0700

    more tests

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Testing!'));
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

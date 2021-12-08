25f65806da1cfce645c8ec824ee36bc16ac084f0
Gabriel Floyd <gtfloyd@a07817b0fe5e.ant.amazon.com>
2021-12-08 13:27:22 -0700

    more comments

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Everybody!'));
 app.get('/', (req, res) => res.send('This is a test.'));
 
 app.listen(port);

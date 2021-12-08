0a53967c5d9e344a3d86fa7c1547941cb55a9409
Floyd <gtfloyd@a07817b0fe5e.ant.amazon.com>
2021-12-08 13:20:19 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hi Everybody!'));
+app.get('/', (req, res) => res.send('This is a test.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

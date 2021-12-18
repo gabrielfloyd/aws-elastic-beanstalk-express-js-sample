1a49a0349fac04ff5aa60efc2768ee139e4e6884
Gabriel Floyd <gtfloyd@amazon.com>
2021-12-18 14:43:09 -0700

    Update #11

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('# # # # # # # # CR3 Outcome: Pending # # # # # # # # # #'));
+// Creating a Date object
+const dateObj = new Date();
+
+app.get('/', (req, res) => res.send('# # # # # # # # CR3 Outcome: Pending # # # # # # # # # #' `Date: ${dateObj.toDateString()}`));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

033062e5e563c58c5030cddd742c885c12e4cd51
Gabriel Floyd <gtfloyd@amazon.com>
2021-12-18 13:58:24 -0700

    Update #6

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('# # # # # # # # CR3 Outcome: Pending # # # # # # # # # #'));
+app.get('/', (req, res) => res.send('# # # # # # # # CR3 Outcome: Pass! # # # # # # # # # # #'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
